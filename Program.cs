da909e0a1c6366417a468cecbb0d3cce4eaff2ec
Xavok <xavoking@gmail.com>
2019-06-19 13:38:10 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
